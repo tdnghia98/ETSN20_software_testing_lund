e1fa86f161038b3d58df4967f1b316372a9903f1
Duy Nghia TRAN <tdnghia98@gmail.com>
2020-11-17 09:23:35 +0100

    lab_2: Add preparation

--- lab_2_black_box/lab_2_preparation.md
+++ lab_2_black_box/lab_2_preparation.md
@@ -18,4 +18,38 @@ Area and perimeter of the triangle are calculated, too.
 
 ### One set of equivalence classes covering valid and invalid cases
 
-Equivalence Class Partioning: Split input/output into classes which the software handles equivalently.•Select test cases to represent each class.
+Equivalence Class Partioning:
+ - Split input/output into classes which the software handles equivalently.
+ - Select test cases to represent each class.
+
+### Side length set
+**Input** : 3 positive whole-number lengths
+ 
+**Output**: description of the triangle
+- <b>equilateral</b> - if all three sides have equal length
+- <b>isosceles</b> - if two sides have equal length
+- <b>right-angled</b> - if one angle is a right angle
+- <b>scalene</b> - all sides different lengths, no right angles
+- <b>impossible</b> - if the given side lengths do not form a triangle
+
+#### Classes
+| Class | Type | Description |
+| ----- | ---- | ----------- |
+| C1 | Input | 3 sides of equal length |
+| C2 | Input | 2 sides of equal length |
+| C3 | Input | All sides of different lengths| 
+| C4 | Input | Lengths does not form a triangle |
+| C5 | Output | "Equilateral" |
+| C6 | Output | "Isosceles" |
+| C7 | Output | "Scalene" |
+| C8 | Output | "Impossible" |
+| C9 | Output | "Right-Angled" |
+| C10 | Output | -1 |
+| C11 | Output | Area of the triangle [number] |
+| C12 | Output | Perimeter of the triangle [number] |
+| C13 | Output | Triangle |
+| C14 | Output | 3 sides' length [string] |
+| C15 | Output | true |
+| C16 | Output | false |
+
+#### Test Cases
--- lab_2_black_box/trianglepackage/TestTriangle.java
+++ lab_2_black_box/trianglepackage/TestTriangle.java
@@ -1,6 +1,72 @@
 package trianglepackage;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TestTriangle {
+import org.junit.jupiter.api.Test;
+
+class Blackbox_test {
+
     @Test
+    void area_test() {
+        Triangle t = new Triangle(2,2,2);
+        assertTrue(t.getArea() == 2);
+
+        Triangle t2 = new Triangle(2,2,5);
+        assertTrue(t2.getArea() == -1);
+    }
+
+    @Test
+    void scalen_test() {
+        Triangle t = new Triangle(2,3,4);
+        assertTrue(t.isScalene());
+
+        Triangle t2 = new Triangle(2, 2, 3);
+        assertFalse(t2.isScalene());
+    }
+
+    @Test
+    void parameter_test() {
+        Triangle t = new Triangle(3,4,5);
+        assertTrue(t.getPerimeter() == 12);
+
+        Triangle t2 = new Triangle(3,4,12);
+        assertFalse(t2.getPerimeter() == 0);
+    }
+
+    @Test
+    void set_side_and_get_side_test() {
+        Triangle t = new Triangle(3,4,5);
+        assertTrue(t.getSideLengths() == "3,4,5");
+
+        t.setSideLengths(1, 2, 2);
+        assertTrue(t.getSideLengths() == "1,2,2");
+    }
+
+    @Test
+    void right_angeled_test() {
+        Triangle t = new Triangle(3,4,5);
+        assertTrue(t.isRightAngled());
+
+        Triangle t2 = new Triangle(3,4,6);
+        assertFalse(t2.isRightAngled());
+    }
+
+    @Test
+    void impossible_triangle_test() {
+        Triangle t = new Triangle(2,2,5);
+        assertTrue(t.isImpossible());
+
+
+        Triangle t2 = new Triangle(2,3,2);
+        assertFalse(t2.isImpossible());
+    }
+
+    @Test
+    void equal_side_test() {
+        Triangle t = new Triangle(2,2,2);
+        assertTrue(t.isEquilateral());
+
+        Triangle t2 = new Triangle(2,3,2);
+        assertFalse(t2.isEquilateral());
+    }
 }
